c6ec8d5a21b037939ac0ffe858ba15f63728e0ea
Frank López <frank.lopez@3pillarglobal.com>
2023-10-26 18:27:44 -0600

    Refactoring the checkout page, using best programming practices in React

--- pay-from-browser/pages/checkout.tsx
+++ pay-from-browser/pages/checkout.tsx
@@ -1,76 +1,76 @@
+import axios from 'axios'
+import { FormEvent, useEffect, useCallback, useState } from "react"
+import { MutatingDots } from "react-loader-spinner"
+
+import { useRouter } from "next/router"
+import Script from 'next/script'
+
 import { useCartState } from "../util/useCartState"
 import { totalPrice, OrderTotal } from "../components/OrderTotal"
 
 import styles from '../styles/Home.module.css'
-
-import Script from 'next/script'
-import axios from 'axios'
-import { FormEvent, useEffect } from "react"
-import { MutatingDots } from "react-loader-spinner"
-import { useRouter } from "next/router"
-
 import { Exact, ExactJSPaymentPayload, ExactPaymentForm } from '../types'
 
+const exactJsSource = `https://${process.env.NEXT_PUBLIC_P2_DOMAIN}/js/v1/exact.js`
+
 export default function Checkout() {
-  let exact: Exact;
+  const [exact, setExact] = useState<Exact | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
 
-  const exactJsSource = `https://${process.env.NEXT_PUBLIC_P2_DOMAIN}/js/v1/exact.js`
-
+  const router = useRouter()
   const items = useCartState().items
 
-  const setOrderPosted = () => {
-    (document.getElementById('paymentForm')! as HTMLInputElement).className = styles.paymentForm;
-    (document.getElementById('loading')! as HTMLInputElement).className = styles.hidden;
-  }
+  const onExactJSReady = useCallback(() => {
+    setIsLoading(false)
 
-  const onExactJSReady = () => {
-    const url = process.env.NEXT_PUBLIC_BASE_URL + '/api/postOrders'
-    axios.post(url, {
+    axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/postOrders`, { 
       amount: totalPrice(items), //Price is in cents
-    }).then(
-      (response) => {
-        exact = ExactJS(response.data.token)
-        const components = exact.components({ orderId: response.data.orderId });
-        components.addCard('cardElement', {
-          label: { position: "above" },
-          style: {
-            default: {
-              padding: '2px',
-              border: "1px solid #ccc",
-              fontSize: "14px",
-            },
+    }).then((response) => {
+      const newExact = ExactJS(response.data.token)
+      const components = newExact.components({ orderId: response.data.orderId })
+
+      components.addCard('cardElement', {
+        label: { position: "above" },
+        style: {
+          default: {
+            padding: '2px',
+            border: "1px solid #ccc",
+            fontSize: "14px",
           },
-          wallets: true
-        });
-
-        exact.on("payment-complete", (payload: unknown) => {
-          const paymentPayload = payload as ExactJSPaymentPayload
-          (document.getElementById('payment_id')! as HTMLInputElement).value = paymentPayload.paymentId;
-          (document.getElementById('myForm') as HTMLFormElement).submit();
-        });
-
-        exact.on("payment-failed", (payload) => {
-          console.debug(payload);
-        });
-        setTimeout(setOrderPosted, 1100);
+        },
+        wallets: true
       })
-  }
 
-  const handleSubmit = (event: FormEvent<ExactPaymentForm>) => {
+      newExact.on("payment-complete", (payload: unknown) => {
+        const paymentPayload = payload as ExactJSPaymentPayload
+
+        (document.getElementById('payment_id')! as HTMLInputElement).value = paymentPayload.paymentId;
+        (document.getElementById('myForm') as HTMLFormElement).submit();
+      })
+
+      newExact.on("payment-failed", (payload) => {
+        console.debug(payload);
+      })
+
+      setExact(newExact)
+    })
+  }, [])
+
+  const handleSubmit = useCallback((event: FormEvent<ExactPaymentForm>) => {
     event.preventDefault()
-    exact.payOrder()
-  }
+    exact?.payOrder()
+  }, [exact])
 
   //Prevent checkout with empty cart
-  const router = useRouter()
   useEffect(() => {
     if (!items) {
       router.push('/')
     }
-  })
+  }, [])
 
   return (
     <>
+      <Script src={exactJsSource} strategy="beforeInteractive" onReady={onExactJSReady} />
       <div className={styles.checkoutdisclaimer}>
         <h1>Demonstration only.</h1>
         <h2>Payments are simulated and no actual funds are transferred.</h2>
@@ -79,54 +79,56 @@ export default function Checkout() {
 
       <main className={styles.main}>
         <OrderTotal />
-        <div id="loading">
-          <MutatingDots height="100" width="100" color="#4fa94d" secondaryColor='#4fa94d' radius='12.5' ariaLabel="mutating-dots-loading" />
+        {(isLoading && (
+          <div>
+            <MutatingDots height="100" width="100" color="#4fa94d" secondaryColor='#4fa94d' radius='12.5' ariaLabel="mutating-dots-loading" />
+          </div>
+        ))}
 
-          <Script src={exactJsSource} strategy="afterInteractive" onReady={onExactJSReady} />
-        </div>
-
-        <div id="paymentForm" className={styles.hidden}>
-          <form id="myForm" action="api/receivePaymentId" method="post" onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="email">Email Address</label>
-              <input type="email" id="email" name="email" autoComplete="email" />
-            </div>
-
-            <div id="cardElement" className={styles.paymentElement}>
-            </div>
-
-            <div>
-              <label htmlFor="address">Address</label>
-              <input type="text" id="address" name="address" autoComplete="street-address" />
-            </div>
-
-            <div>
-              <label htmlFor="apartment">Apartment, suite, etc.</label>
-              <input type="text" id="apartment" name="apartment" />
-            </div>
-
-            <div>
-              <label htmlFor="city">City</label>
-              <input type="text" id="city" name="city" />
-            </div>
-
-            <div>
-              <label htmlFor="province">State</label>
-              <input type="text" id="province" name="province" />
-            </div>
-
-            <div>
-              <label htmlFor="postcode">Postal code</label>
-              <input type="text" id="postcode" name="postcode" autoComplete="postal-code" />
-            </div>
-
-            <input type="hidden" name="payment_id" id="payment_id"></input>
-
-            <div>
-              <input type="submit" name="commit" value="Pay Now" data-disable-with="Pay Now" />
-            </div>
-          </form>
-        </div>
+        {(!isLoading && (
+          <div id="paymentForm" className={styles.paymentForm}>
+            <form id="myForm" action="api/receivePaymentId" method="post" onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="email">Email Address</label>
+                <input type="email" id="email" name="email" autoComplete="email" />
+              </div>
+  
+              <div id="cardElement" className={styles.paymentElement}>
+              </div>
+  
+              <div>
+                <label htmlFor="address">Address</label>
+                <input type="text" id="address" name="address" autoComplete="street-address" />
+              </div>
+  
+              <div>
+                <label htmlFor="apartment">Apartment, suite, etc.</label>
+                <input type="text" id="apartment" name="apartment" />
+              </div>
+  
+              <div>
+                <label htmlFor="city">City</label>
+                <input type="text" id="city" name="city" />
+              </div>
+  
+              <div>
+                <label htmlFor="province">State</label>
+                <input type="text" id="province" name="province" />
+              </div>
+  
+              <div>
+                <label htmlFor="postcode">Postal code</label>
+                <input type="text" id="postcode" name="postcode" autoComplete="postal-code" />
+              </div>
+  
+              <input type="hidden" name="payment_id" id="payment_id"></input>
+  
+              <div>
+                <input type="submit" name="commit" value="Pay Now" data-disable-with="Pay Now" />
+              </div>
+            </form>
+          </div>
+        ))}
       </main>
     </>
   )
